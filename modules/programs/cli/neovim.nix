04350905d1a242d0b3400125d54e5ed5351a56f3
Tobias Dols <tobiasdols@gmail.com>
2026-02-05 16:34:22 +0100

    set dotfiles for nvim

--- modules/programs/cli/neovim.nix
+++ modules/programs/cli/neovim.nix
@@ -1,7 +1,30 @@
 {
-  flake.nixosModules.neovim = {pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-      neovim
-    ];
+  flake.nixosModules.neovim = {config, pkgs, ... }: {
+
+
+
+  home-manager.users.tobsend = {config, ...}: {
+      stylix.targets.neovim.enable = false;
+      programs.neovim.enable = true;
+      home = {
+        packages = with pkgs; [
+          statix
+          ripgrep
+          nil
+          nixpkgs-fmt
+          gcc
+        ];
+
+        sessionVariables = {
+          EDITOR = "nvim";
+        };
+
+      };
+
+      xdg.configFile."nvim" = {
+        source = config.lib.file.mkOutOfStoreSymlink "/home/tobsend/nix-dotfiles/nvim/";
+        recursive = true;
+      };
+    };
   };
 }
