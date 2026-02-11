af24fa957d8245eceb24a1ef2889ce441100da04
Tobias Dols <tobiasdols@gmail.com>
2026-02-11 11:15:04 +0100

    cleanup gnome

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -7,5 +7,18 @@
         displayManager.gdm.enable = true;
         desktopManager.gnome.enable = true;
       };
+
+      environment.gnome.excludePackages = (
+        with pkgs;
+        [
+          atomix # puzzle game
+          epiphany # web browser
+          evince # document viewer
+          gnome-tour
+          hitori # sudoku game
+          iagno # go game
+          tali # poker game
+        ]
+      );
     };
 }
